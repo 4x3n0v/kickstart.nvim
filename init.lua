09c62c33bf6ddd4143c03f4ebbe1f4f2f24b2d64
Oleg <oaksenov@nntc.pro>
2026-04-02 19:15:28 +0700

    feat: open filebrowser from file's folder

--- init.lua
+++ init.lua
@@ -324,7 +324,12 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-      vim.keymap.set('n', '<leader>sa', ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>", { desc = '[S]earch by Grep [A]rgs' })
+      vim.keymap.set('n', '<leader>sa', function()
+        require('telescope').extensions.live_grep_args.live_grep_args()
+      end, { desc = '[S]earch by Grep [A]rgs' })
+      vim.keymap.set('n', '<leader>fh', function()
+        require('telescope').extensions.file_browser.file_browser { path = '%:p:h' }
+      end, { desc = 'Open [F]ile browser [H]ere' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
