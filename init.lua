b6cd5bbdecea30bcb51d608a1e865391a121e1b5
Oleg <oaksenov@nntc.pro>
2026-04-06 00:18:28 +0700

    WIP: cant fix highlighting

--- init.lua
+++ init.lua
@@ -100,7 +100,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.highlight.on_yank()
+    vim.hl.on_yank()
   end,
 })
 
@@ -130,7 +130,7 @@ local plugins = {
     end,
   },
   {
-    src = 'https://github.com/mason-org/mason-tool-installer.nvim',
+    src = 'https://github.com/WhoIsSethDaniel/mason-tool-installer.nvim',
     setup = function()
       require('mason-tool-installer').setup {
         ensure_installed = {
@@ -141,20 +141,22 @@ local plugins = {
           'pyright',
         },
       }
-      vim.keymap.set('n', '<leader>o', '<cmd>LspClangdSwitchSourceHeader<CR>', { desc = 'Switch between header/source for cpp' })
+      vim.keymap.set('n', '<leader>o', '<cmd>LspClangdSwitchSourceHeader<CR>', {
+        desc = 'Switch between header/source for cpp',
+      })
     end,
   },
   {
     src = 'https://github.com/j-hui/fidget.nvim',
-    setup = function() end,
-  },
-  {
-    src = 'https://github.com/hrsh7th/cmp-nvim-lsp',
-    setup = function() end,
+    setup = function()
+      require('fidget').setup {}
+    end,
   },
   {
     src = 'https://github.com/saadparwaiz1/cmp_luasnip',
-    setup = function() end,
+    setup = function()
+      require('luasnip').setup {}
+    end,
   },
   {
     src = 'https://github.com/L3MON4D3/LuaSnip',
@@ -170,7 +172,16 @@ local plugins = {
   },
   {
     src = 'https://github.com/hrsh7th/cmp-nvim-lsp',
-    setup = function() end,
+    setup = function()
+      require('cmp').setup {
+        sources = {
+          { name = 'nvim_lsp' },
+        },
+      }
+
+      -- The nvim-cmp almost supports LSP's capabilities so You should advertise it to LSP servers..
+      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+    end,
   },
   {
     src = 'https://github.com/hrsh7th/cmp-path',
@@ -231,9 +242,72 @@ local plugins = {
     end,
   },
   {
+    src = 'https://github.com/folke/lazydev.nvim',
+    setup = function()
+      require('lazydev').setup {}
+    end,
+  },
+  {
     src = 'https://github.com/neovim/nvim-lspconfig',
     setup = function()
-      vim.lsp.enable { 'lua_ls', 'rust_analyzer' }
+      vim.lsp.enable { 'lua_ls', 'rust_analyzer', 'clangd' }
+      vim.api.nvim_create_autocmd('LspAttach', {
+        group = vim.api.nvim_create_augroup('kickstart-lsp-attach', { clear = true }),
+        callback = function(event)
+          local map = function(keys, func, desc, mode)
+            mode = mode or 'n'
+            vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = 'LSP: ' .. desc })
+          end
+          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction', { 'n', 'x' })
+          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+
+          -- The following two autocommands are used to highlight references of the
+          -- word under your cursor when your cursor rests there for a little while.
+          --    See `:help CursorHold` for information about when this is executed
+          --
+          -- When you move your cursor, the highlights will be cleared (the second autocommand).
+          local client = vim.lsp.get_client_by_id(event.data.client_id)
+          if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight, event.buf) then
+            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+            vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+              buffer = event.buf,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.document_highlight,
+            })
+
+            vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+              buffer = event.buf,
+              group = highlight_augroup,
+              callback = vim.lsp.buf.clear_references,
+            })
+
+            vim.api.nvim_create_autocmd('LspDetach', {
+              group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+              callback = function(event2)
+                vim.lsp.buf.clear_references()
+                vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+              end,
+            })
+          end
+
+          -- The following code creates a keymap to toggle inlay hints in your
+          -- code, if the language server you are using supports them
+          --
+          -- This may be unwanted, since they displace some of your code
+          if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_inlayHint, event.buf) then
+            map('<leader>th', function()
+              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled { bufnr = event.buf })
+            end, '[T]oggle Inlay [H]ints')
+          end
+        end,
+      })
     end,
   },
   {
@@ -246,6 +320,8 @@ local plugins = {
         },
         formatters_by_ft = {
           lua = { 'stylua' },
+          rust = { 'rustfmt' },
+          cpp = { 'clang-format' },
         },
       }
     end,
@@ -269,8 +345,10 @@ local plugins = {
   },
   {
     src = 'https://github.com/nvim-treesitter/nvim-treesitter',
+    version = 'main',
     build = ':TSUpdate',
     setup = function()
+      require('nvim-treesitter').setup {}
       require('nvim-treesitter').install {
         'bash',
         'diff',
@@ -284,6 +362,13 @@ local plugins = {
         'cpp',
         'python',
       }
+
+      vim.api.nvim_create_autocmd('FileType', {
+        pattern = { '<filetype>' },
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
+      })
     end,
   },
   {
